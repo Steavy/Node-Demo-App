861b66b5c2ebc729dd5dc7c2a6d106f8470328ba
killer0014 <lars@gmx.com>
2023-04-11 15:58:05 +0200

    test2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World New Docker Image!';
+    return 'Hello World New Docker Image2!';
   }
 }
