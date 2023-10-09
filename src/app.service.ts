d604aa3dd2a1cfdbbe7881bdc1663047c1e73437
Steven <Steavy@users.noreply.github.com>
2023-10-09 14:15:41 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World New Docker Image break code for demo purpose2!';
+    return 'Hello World New Docker Image!';
   }
 }
